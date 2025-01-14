79fe1ffb7cb654471116b0dc32c20abfca5f992f
Amanda Beiner <84938829+heyitsab@users.noreply.github.com>
2025-01-14 10:49:36 -0500

    Add checks for 'puts' and 'binding.pry' statements

--- alpaca_test.rb
+++ alpaca_test.rb
@@ -1,1 +1,39 @@
+class PutsStatementFoundError < StandardError; end
+class BindingPryFoundError < StandardError; end
 
+def check_files_for_puts(files)
+  files.each do |file|
+    if File.exist?(file)
+      File.foreach(file).with_index do |line, line_num|
+        # Match `puts` method using a regular expression
+        if line.match?(/^\s*puts\s+/)
+          raise PutsStatementFoundError, "File '#{file}' contains 'puts' on line #{line_num + 1}"
+        end
+      end
+    else
+      puts "File '#{file}' does not exist."
+    end
+  end
+  puts "No 'puts' statements found in the provided files."
+end
+
+def check_files_for_binding_pry(files)
+  files.each do |file|
+    if File.exist?(file)
+      File.foreach(file).with_index do |line, line_num|
+        # Match `binding.pry` using a regular expression
+        if line.match?(/^\s*binding\.pry\s*/)
+          raise BindingPryFoundError, "File '#{file}' contains 'binding.pry' on line #{line_num + 1}"
+        end
+      end
+    else
+      puts "File '#{file}' does not exist."
+    end
+  end
+  puts "No 'binding.pry' statements found in the provided files."
+end
+
+files_to_check = ['alpaca.rb']
+
+check_files_for_puts(files_to_check)
+check_files_for_binding_pry(files_to_check)
